0dcec8c2f5a8e2d4ea21a66b38481695f138e55e
Josué Dos Santos Domingues <92592495+Josue185@users.noreply.github.com>
2023-06-26 11:59:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,11 +15,19 @@ const handleSubmit = (event) => {
     
     const Agente = document.querySelector("#Agente").value;
     const LDAP = document.querySelector("#LDAP").value;
-    const dataAtendimento = document.querySelector("#dataAtendimento").value;
-    const issue = document.querySelector("#issue").value;
+    const cognizantID = document.querySelector("#cogId").value;
+    const tL = document.querySelector("#teamLeader").value;
+    const dataOfAbstance = document.querySelector("#dataOfAbsance").value;
+    const dataOfReturn = document.querySelector("#dateOfReturn").value;
+    const totalDay = document.querySelector("#totalDay").value;
+    const bussinesDay = document.querySelector("#bussinesDay").value;
+    const reason = document.querySelector("#reason").value;
+    const type = document.querySelector("#type").value;
+    const documentation = document.querySelector("#documentation").value;
+    const month = document.querySelector("#month").value;
+    const year = document.querySelector("#documentation").value;
+    const comments = document.querySelector("#comments").value;
     const screenshoot = document.querySelector("#screenshoot").value;
-    const status = document.querySelector("#status").value;
-    const observacao = document.querySelector("#observacao").value;
     
 
     fetch('https://api.sheetmonkey.io/form/jzi1PQfx9zyXR7TjiMHLzN', {
@@ -28,12 +36,12 @@ const handleSubmit = (event) => {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({Agente, LDAP, issue, screenshoot, dataAtendimento, status, observacao})
+        body: JSON.stringify({Agente, LDAP, cognizantID: "Cognizant ID", tL:"Team Leader", dataOfAbstance: "Date of Abstance", dataOfReturn:"Date of Return", totalDay:"Total Day", month:"Month", year:"Year", comments:"Comments", screenshoot:"SS do bloqueio no cases"})
 
  })
   .then(() => {
         removeLoading();
-        document.querySelector('input[name="aldapAgente"]').value = '';
+        document.querySelector("#Agente").value = '';
         document.querySelector("#dataAtendimento").value = '';
         document.querySelector("#issue").value = '';
         document.querySelector("#screenshoot").value = '';
